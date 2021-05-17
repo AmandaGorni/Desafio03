e1d936ad913050d9cade2c817f242b3c716821ba
AmandaGorni <ap.gorni@gmail.com>
2021-05-17 00:34:10 -0300

    Desafio 03 finalizado

---
+++ index.js
@@ -0,0 +1,27 @@
+
+// 1 - Criar um loop que conte de 1 até 10 usando FOR
+for (let x = 1; x<=10; x++){
+    console.log(x)
+}
+
+// 2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+let y = 10
+
+while(y>=1){
+    console.log(y)
+    y--
+}
+
+// 3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+for(w=1;w<=100;w+=2){
+    console.log(w)
+}
+
+// 4- Criar um loop que conte todos os números pares de 0 a 100
+
+for(p=0;p<=100;p+=2){
+    console.log(p)
+}
+
